90ce24e2115965633991599251123412c5e0175c
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-11-14 17:29:43 +0900

    Time: 0 ms (100.00%), Space: 43.4 MB (47.53%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return binarySearch(nums, target);
+    }
+    
+    public int binarySearch(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] < target) l = m + 1;
+            else r = m;
+        }
+        return l;
+    }
+}
