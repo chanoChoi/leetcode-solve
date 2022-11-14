7047e45294ae50bc9003ca7cb199cf798d472b06
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-11-14 17:31:28 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (63.36%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -7,7 +7,7 @@ class Solution {
         int l = 0;
         int r = nums.length;
         while (l < r) {
-            int m = (l + r) / 2;
+            int m = l + (r - l) / 2;
             if (nums[m] < target) l = m + 1;
             else r = m;
         }
