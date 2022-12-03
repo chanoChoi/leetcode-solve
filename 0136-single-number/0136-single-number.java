bdd70406d1c12bb0c2fec33d08c01705b437e8e7
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-03 14:04:18 +0900

    Time: 61 ms (8.74%), Space: 54.5 MB (7.82%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,10 +2,26 @@ class Solution {
     public int singleNumber(int[] nums) {
         // set TC = O(n) SC = O(n)
 //         xor TC = O(n) SC = O(1)
-        int res = 0;
+        Set<Integer> answer = new HashSet<>();
+        Set<Integer> duplicate = new HashSet<>();
+        
         for (int n : nums) {
-            res ^= n;
+            if (answer.contains(n)) {
+                duplicate.add(n);
+            } else {
+                answer.add(n);
+            }
         }
-        return res;
+        
+        answer.removeAll(duplicate);
+        
+        return answer.iterator().next();
+        
+        // **중복체크**를 위한 주머니 생성(set 자료구조를 사용) 
+        // 주어진 배열을 순회하며 배열내의 수를 조회한다.
+        // 중복체크 주머니내에 수가 존재하지 않는다면 체크용 주머니에 삽입
+        // 중복체크주머니내에 수가 존재한다면 답 주머니에 삽입
+        
+        // 중복체크 주머니와 답 주머니의 차집합을 구한다.
     }
 }
