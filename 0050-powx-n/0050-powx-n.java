d4d0cbfd0a698a455b150c1d842bc2636bbded54
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-03 14:54:41 +0900

    Time: 1 ms (86.49%), Space: 43.4 MB (30.03%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
+        // overflow
         long num = n;
+        double ans = 1.0;
+        boolean minus = false;
         if (n < 0) {
             num = num * -1;
+            minus = true;
         }
-        double ans = 1.0;
         while (num > 0) {
-           if (num % 2 == 0) {
-               x = x * x;
-               num = num / 2;
-           } else {
-               ans = ans * x;
-               num = num - 1;
-           }
+            if (num % 2 == 0) {
+                x = x * x;
+                num = num / 2;
+            } else {
+                ans = ans * x;
+                num = num - 1;
+            }
         }
         
-        return n < 0 ? 1.0/ans : ans;
+        return minus ? 1/ans : ans;
     }
 }
