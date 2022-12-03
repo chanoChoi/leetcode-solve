91e5608c7a3cc3886ed8cfe23a9508a241b436fc
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-03 14:46:54 +0900

    Time: 1 ms (85.98%), Space: 41.4 MB (32.45%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,6 +1,5 @@
 class Solution {
     public int trailingZeroes(int n) {
-        // 10 == 2 * 5  2가 더 많다. 5가 몇개나 나오는지 확인. 
         int cnt = 0;
         int idx = 5;
         while (idx <= n) {
@@ -8,5 +7,6 @@ class Solution {
             idx = idx * 5;
         }
         return cnt;
+        
     }
 }
