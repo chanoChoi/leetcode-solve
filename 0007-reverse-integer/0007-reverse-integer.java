65bfb84b260bb585e4482d6799d7103de71bcd7d
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-03 14:17:17 +0900

    Time: 2 ms (83.63%), Space: 41.2 MB (66.01%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,20 +1,13 @@
 class Solution {
     public int reverse(int x) {
-        int reverse = 0;
-        int max = Integer.MAX_VALUE;
         int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        int reverse = 0;
         
         while (x != 0) {
             int lastDigit = x % 10;
-            
-            if (reverse > max / 10 || (reverse == max / 10 && lastDigit > 7)) {
-                return 0;
-            }
-            
-            if (reverse < min / 10 || (reverse == min / 10 && lastDigit < -8)) {
-                return 0;
-            }
-            
+            if (reverse > max / 10 || (reverse == max / 10 && lastDigit > 7)) return 0;
+            if (reverse < min / 10 || (reverse == min / 10 && lastDigit < -8)) return 0;
             reverse = reverse * 10 + lastDigit;
             x = x / 10;
         }
@@ -22,3 +15,20 @@ class Solution {
         return reverse;
     }
 }
+
+// 숫자를 뒤집는데 뒤집어서 만드는 수가 2^31 - 1 보다 크거나 -2^31 보다 작으면 0을 반환
+
+// 비교용 MAX_VALUE
+// 비교용 MIN_VALUE
+// 반환할 뒤집은 수;
+
+// 주어진 x가 0아니라면 특정 로직을 반복
+// 주어진 수의 가장 마지막 자리의 수를 가져온다. 모듈러
+// 현재 만들어진 뒤집은 수가 MAX의 뒷자리를 제외한 수보다 크다면 뒤에 어떤 수가 오던지 범위를 넘게된다 return 0
+// 현재 만들어진 뒤집은 수가 MAX의 뒷자리를 제외한 수와 같다면 마지막 수인 7 보다 크면 범위를 넘게된다 return 0
+
+// 현재 만들어진 뒤집은 수가 MIN의 뒷자리를 제외한 수보다 작다면 뒤에 어떤 수가 오던지 범위를 넘게된다 return 0
+// 현재 만들어진 뒤집은 수가 MIN의 뒷자리를 제외한 수와 같다면 MIN의 마지막 수인 -8 보다 작다면 범위를 넘게된다 R 0
+
+// 뒤집은 수 = 뒤집은 수 * 10 + 마지막 자리의 수
+// 주어진 수를 업데이트 #마지막수제거
