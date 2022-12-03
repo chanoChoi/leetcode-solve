5d22dcdbef30eb3256f28d5e13f75485b32612a2
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-03 13:49:58 +0900

    Time: 1 ms (100.00%), Space: 43.3 MB (89.04%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,21 +1,17 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        String FIZZBUZZ = "FizzBuzz";
-        String FIZZ = "Fizz";
-        String BUZZ = "Buzz";
-        
-        List<String> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                result.add(FIZZBUZZ);
-            } else if (i % 3 == 0) {
-                result.add(FIZZ);
-            } else if (i % 5 == 0) {
-                result.add(BUZZ);
-            } else {
-                result.add(String.valueOf(i));
-            }
+        List<String> answer = new ArrayList<>();
+        // 1부터 주어진 수 까지 반복
+        for (int i = 1; i <= n; i++){
+        // 수를 3 또는 5 나누었을 떄 나머지가 0 이면 "FizzBuzz"를 배열에 삽입
+            if (i % 3 == 0 && i % 5 == 0) answer.add("FizzBuzz");
+        // 수를 3으로 나누었을 때 나머지가 0 이면 "Fizee"를 배열에 삽입
+            else if (i % 3 == 0)answer.add("Fizz");
+        // 수를 5로 나우었을 때 나머지가 0 이면 "Buzz"를 배열에 삽입
+            else if (i % 5 == 0) answer.add("Buzz");
+//          위 조건들을 만족하지 않는 수는 숫자를 배열에 삽입
+            else answer.add(String.valueOf(i));
         }
-        return result;
+        return answer;
     }
 }
