91adca933d72d7d0f160d684a46d73506352e153
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-11 14:22:43 +0900

    Time: 18 ms (90.79%), Space: 51 MB (87.80%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> duplication = new HashSet<>();
-        Set<String> ans = new HashSet<>();
-        int len = s.length();
-        for (int i = 0; i <= len - 10; i++) {
-               String substr = s.substring(i, i + 10);
-            if (duplication.contains(substr)) {
-                ans.add(substr);
+        Set<String> dup = new HashSet<>();
+        Set<String> answer = new HashSet<>();
+        int idx = 0;
+        while (idx + 10 <= s.length()) {
+            String subStr = s.substring(idx, idx + 10);
+            if (dup.contains(subStr)) {
+                answer.add(subStr);
             } else {
-                duplication.add(substr);
+                dup.add(subStr);
             }
+            idx = idx + 1;
         }
-        return ans.stream().collect(Collectors.toList());
+        return answer.stream().collect(Collectors.toList());
     }
 }
