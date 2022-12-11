9a3286d5a11982a07dff2d35c8408ea9a8f60282
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-11 15:42:36 +0900

    Time: 14 ms (49.37%), Space: 43.7 MB (55.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,19 +5,13 @@ class Solution {
         int[] chars = new int[26];
         for (char ch : s.toCharArray()) {
             chars[ch - 'a']++;
-        }        
+        }
         for (char ch : t.toCharArray()) {
             chars[ch - 'a']--;
-            if (chars[ch - 'a'] < 0) return false;
-        }        
-        
+            if (chars[ch - 'a'] < 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
-
-/*
-for (int i = 0; i < s.length(); i++) {
-            chars[s.charAt(i) - 'a']++;
-            chars[t.charAt(i) - 'a']--;
-}
-        */
