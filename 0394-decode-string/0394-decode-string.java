3074a775a2cb5cb194d39ab3388d2352dd663e3d
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-11 14:56:58 +0900

    Time: 1 ms (93.70%), Space: 42.4 MB (24.99%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,32 +1,33 @@
 class Solution {
     public String decodeString(String s) {
-        Deque<Integer> numStack = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
         Deque<String> strStack = new LinkedList<>();
-        StringBuilder ans = new StringBuilder();
+        Deque<Integer> numStack = new LinkedList<>();
+        
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (Character.isDigit(ch)) {
                 int num = ch - '0';
                 while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
-                    int lastDigit = s.charAt(i + 1) - '0';
-                    num = num * 10 + lastDigit;
+                    int digit = s.charAt(i + 1) - '0';
+                    num = num * 10 + digit;
                     i = i + 1;
                 }
                 numStack.addLast(num);
             } else if (ch == '[') {
-                strStack.addLast(ans.toString());
-                ans = new StringBuilder();
+                strStack.addLast(sb.toString());
+                sb = new StringBuilder();
             } else if (ch == ']') {
                 int k = numStack.removeLast();
                 StringBuilder tmp = new StringBuilder(strStack.removeLast());
                 for (int j = 0; j < k; j++) {
-                    tmp.append(ans);
+                    tmp.append(sb);
                 }
-                ans = tmp;
+                sb = tmp;
             } else {
-                ans.append(ch);
+                sb.append(ch);
             }
         }
-        return ans.toString();
+        return sb.toString();
     }
 }
