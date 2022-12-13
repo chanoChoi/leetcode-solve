02dca64da9f9526fa5173092f7efc0c7ef963c66
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-13 13:40:10 +0900

    Time: 5 ms (94.31%), Space: 42 MB (86.85%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> answer = new ArrayList<>();
+        answer.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            int cur = nums[i];
+            if (answer.get(answer.size() - 1) < cur) {
+                answer.add(cur);
+            } else {
+                int targetIdx = binarySearch(answer, cur);
+                answer.set(targetIdx, cur);
+            }
+        }
+        return answer.size();
+    }
+//     현재 target에 가장 가까운 수
+    private int binarySearch(List<Integer> nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (target > nums.get(m)) l = m + 1;
+            else r = m;
+        }
+        return r;
+    }
+}
