fe4d16a043dd9c7550bb86b4d58f6b81ca554a2e
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-13 19:51:59 +0900

    Time: 45 ms (8.45%), Space: 71.9 MB (13.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        boolean result = false;
-        
-        Set<Integer> set = new HashSet<>();
-        
-        for (int n : nums) {
-            if (set.contains(n)) {
-                result = true;
-                break;
-            } else {
-                set.add(n);
-            }
-        }
-        
-        return result;
+        return Arrays.stream(nums).boxed().collect(Collectors.toSet()).size() != nums.length;
     }
 }
