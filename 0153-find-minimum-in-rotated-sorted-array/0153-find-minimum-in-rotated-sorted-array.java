2dc45be402569de92ce02d2f25bc9eadba8e4921
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-18 14:04:19 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (64.81%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,20 +1,18 @@
 class Solution {
     public int findMin(int[] nums) {
-        int len = nums.length;
-        if (len == 1) return nums[0];
-        if (len == 2) return Math.min(nums[0], nums[1]);
-        if (nums[0] < nums[len - 1]) return nums[0];
-        int l = 0, r = len - 1;
-        int answer = 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.min(nums[0], nums[1]);
+        if (nums[0] < nums[nums.length - 1]) return nums[0];
+        
+        int l = 0, r = nums.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (nums[m] > nums[m + 1]) {
-                answer = nums[m + 1];
-                break;
-            }
+            if (m + 1 < nums.length && nums[m] > nums[m + 1]) return nums[m + 1];
+            else if (m - 1 >= 0 && nums[m] < nums[m - 1]) return nums[m];
+            
             if (nums[l] <= nums[m]) l = m + 1;
             else if (nums[r] >= nums[m]) r = m - 1;
         }
-        return answer;
+        return nums[l];
     }
 }
