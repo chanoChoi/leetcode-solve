02968c66776d30d039125cb9b22aa35fca9149b6
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-18 13:44:08 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (91.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -4,12 +4,11 @@ class Solution {
         while (l <= r) {
             int m = l + (r - l) / 2;
             if (nums[m] == target) return m;
-            
             if (nums[l] <= nums[m]) {
                 if (nums[l] <= target && target < nums[m]) r = m - 1;
                 else l = m + 1;
             } else {
-                if (nums[m] < target && target <= nums[r]) l = m + 1;
+                if (nums[r] >= target && target > nums[m]) l = m + 1;
                 else r = m - 1;
             }
         }
