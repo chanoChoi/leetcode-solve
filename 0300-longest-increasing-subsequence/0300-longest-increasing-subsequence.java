9de9839a2060aa2ea78c5efcccf3e7d1f87f4743
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-18 14:37:22 +0900

    Time: 5 ms (93.73%), Space: 41.9 MB (85.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> answer = new ArrayList<>();
-        answer.add(nums[0]);
-        for (int i = 1;i < nums.length; i++) {
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
             int cur = nums[i];
-            if (answer.get(answer.size() - 1) < cur) {
-                answer.add(cur);
+            if (list.get(list.size() - 1) < cur) {
+                list.add(cur);
             } else {
-                int targetIdx = binarySearch(answer, cur);
-                answer.set(targetIdx, cur);
+                int targetIdx = binarySearch(list, cur);
+                list.set(targetIdx, cur);
             }
         }
-        return answer.size();
+        return list.size();
     }
     
-    private int binarySearch(List<Integer> list,int target) {
-        int l = 0, r = list.size() - 1;
+    private int binarySearch(List<Integer> nums, int target) {
+        int l = 0, r = nums.size() - 1;
         while (l < r) {
             int m = l + (r - l) / 2;
-            if (target > list.get(m)) l = m + 1;
+            if (target > nums.get(m)) l = m + 1;
             else r = m;
         }
-        return r;
-    }
+        return l;
+    } 
 }
