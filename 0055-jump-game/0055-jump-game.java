e7a75e090795f45200e5d983725509781942077b
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-19 14:00:14 +0900

    Time: 5 ms (63.30%), Space: 68.6 MB (17.54%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+        for (int i = 0; i < nums.length && reachable >= i; i++) {
+            reachable = Math.max(reachable, i + nums[i]);
+            if (reachable >= nums.length - 1) return true;
+        }
+        return false;
+    }
+}
