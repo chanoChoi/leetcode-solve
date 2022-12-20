ec5b42ac406484b1b25ef08668f617605203edc7
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-20 10:28:14 +0900

    Time: 1 ms (24.20%), Space: 42.6 MB (32.35%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        Deque<Integer> list = new LinkedList<>();
         int carry = 1;
         for (int i = digits.length - 1; i >= 0; i--) {
             int sum = digits[i] + carry;
             carry = sum / 10;
-            list.addFirst(sum % 10);
+            digits[i] = sum % 10;
         }
-        if (carry == 1) list.addFirst(1);
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        
+        if (carry == 1) {
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+        return digits;
     }
 }
