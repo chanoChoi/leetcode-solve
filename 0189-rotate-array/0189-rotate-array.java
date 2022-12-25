89800b88c1b1ee85049843c1a1a466097da7a347
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-25 17:32:11 +0900

    Time: 0 ms (100.00%), Space: 58.5 MB (80.96%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,29 +1,18 @@
 class Solution {
-    private <T> void p(T t) {
-        System.out.print(t);
-    }
-    
-    private <T> void pln(T t) {
-        System.out.println(t);
-    }
-    
     public void rotate(int[] nums, int k) {
-        if (k > nums.length) k = k % nums.length;
-        // 1. reverse the while array
-            reverse(nums, 0, nums.length - 1);
-         // 2. reverse the array k 만큼
-            reverse(nums, 0, k - 1);    
-        // 3. reverse the array other value
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
         reverse(nums, k, nums.length - 1);
     }
     
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int tmp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = tmp;
-            left = left + 1;
-            right = right - 1;
+    private void reverse(int[] nums, int idx1, int idx2) {
+        while (idx1 < idx2) {
+            int tmp = nums[idx1];
+            nums[idx1] = nums[idx2];
+            nums[idx2] = tmp;    
+            idx1 = idx1 + 1;
+            idx2 = idx2 - 1;
         }
     }
 }
