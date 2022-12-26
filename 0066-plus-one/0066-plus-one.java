54727e55b8f4629d4ccc1cd203d31d1b03e6be03
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-26 11:18:18 +0900

    Time: 0 ms (100.00%), Space: 41 MB (76.90%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        int[] answer = new int[digits.length + 1];
         int carry = 1;
         for (int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
-            carry = sum / 10;
-            digits[i] = sum % 10;
+            int lastDigit = digits[i];
+            int tmp = lastDigit + carry;
+            carry = tmp / 10;
+            digits[i] = tmp % 10;
         }
         if (carry == 1) {
-            int[] newDigits = new int[digits.length + 1];
-            newDigits[0] = 1;
-            return newDigits;
+            System.arraycopy(digits, 0, answer, 1, digits.length);
+            answer[0] = 1;
+            
         }
-        return digits;
+        return answer[0] == 1 ? answer : digits;
     }
 }
