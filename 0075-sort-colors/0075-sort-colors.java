417f4f575b6527418f277d6b310d6c7fa9e5c4fd
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-26 11:05:34 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (90.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,19 +3,19 @@ class Solution {
         int l = 0, m = 0, r = nums.length - 1;
         while (m <= r) {
             if (nums[m] == 0) {
-                swap(l, m, nums);
-                m = m + 1;
+                swap(nums, l, m);
                 l = l + 1;
+                m = m + 1;
             } else if (nums[m] == 1) {
                 m = m + 1;
-            } else {
-                swap(m, r, nums);
+            } else if (nums[m] == 2) {
+                swap(nums, m, r);
                 r = r - 1;
             }
         }
     }
     
-    private void swap(int idx1, int idx2, int[] nums) {
+    private void swap(int[] nums, int idx1, int idx2) {
         int tmp = nums[idx1];
         nums[idx1] = nums[idx2];
         nums[idx2] = tmp;
