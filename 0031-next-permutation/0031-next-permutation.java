097bf45b215a31ace00c47cbcdb321267924dcad
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2022-12-27 10:39:52 +0900

    Time: 0 ms (100.00%), Space: 42.4 MB (93.54%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,43 +1,30 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        boolean flag = false;
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                flag = true;
-                int targetIdx = find(nums, nums[i - 1], i, nums.length - 1);
-                swap(nums, i - 1, targetIdx);
-                reverse(nums, i);
-                break;
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i = i - 1;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[i] >= nums[j]) {
+                j--;
             }
+            swap(nums, i, j);
         }
-        
-        if (!flag) {
-            Arrays.sort(nums);
-        }
-    }
-    private int find(int[] nums, int target,int start, int end) {
-        int index = -1;
-        for (int i = end; i >= start; i--) {
-            if (target < nums[i]) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    }
-    
-    private void reverse(int[] nums, int l) {
-        int r = nums.length - 1;
-        while (l < r) {
-            swap(nums, l, r);
-            l = l + 1;
-            r = r - 1;
-        }
+        reverse(nums, i + 1, nums.length - 1);
     }
     
     private void swap(int[] nums, int idx1, int idx2) {
         int tmp = nums[idx1];
         nums[idx1] = nums[idx2];
         nums[idx2] = tmp;
+    }
+    
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left = left + 1;
+            right = right - 1;
+        }
     }
 }
