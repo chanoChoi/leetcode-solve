430d96f8016438814640aed148dc350df8f44c30
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2023-01-07 14:00:17 +0900

    Time: 3 ms (98.82%), Space: 42.4 MB (87.65%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -2,47 +2,107 @@ class Solution {
     public String reverseWords(String s) {
         char[] chars = s.toCharArray();
         reverse(chars, 0, chars.length - 1);
-        reverseWord(chars);
-        return removeSpaces(chars);
+        reverseWords(chars);
+        return cleanSpaces(chars);
     }
-    private String removeSpaces(char[] chars) {
-        int left = 0, right = 0;
-        int len = chars.length;
-        while (right < len) {
-            while (right < len && chars[right] == ' ') right = right + 1;
-            while (right < len && chars[right] != ' ') {
+    
+    private String cleanSpaces(char[] chars) {
+        int left = 0;
+        int right = 0;
+        while (right < chars.length) {
+            while (right < chars.length && chars[right] == ' ') right = right + 1;
+            while (right < chars.length && chars[right] != ' ') {
                 chars[left] = chars[right];
                 left = left + 1;
                 right = right + 1;
             }
-            while (right < len && chars[right] == ' ') right = right + 1;
-            if (right < len) {
+            while (right < chars.length && chars[right] == ' ') right = right + 1;
+            if (right < chars.length) {
                 chars[left] = ' ';
                 left = left + 1;
             }
         }
-        return String.valueOf(chars).substring(0, left);
+        return new String(chars).substring(0, left);
     }
     
-    private void reverseWord(char[] chars) {
-        int left = 0, right = 0;
-        int len = chars.length;
-        while (right < len) {
-            while (left + 1 < len && chars[left] == ' ') left = left + 1;
+    private void reverseWords(char[] chars) {
+        int left = 0;
+        int right = 0;
+        while (right < chars.length) {
+            while (left < chars.length && chars[left] == ' ') {
+                left = left + 1;
+            }
             right = left;
-            while (right + 1 < len && chars[right + 1] != ' ') right = right + 1;
-            reverse(chars, left, right);
-            left = right + 1;
+            while (right < chars.length && chars[right] != ' ') {
+                right = right + 1;
+            }
+            reverse(chars, left, right - 1);
+            left = right;
         }
     }
     
     private void reverse(char[] chars, int left, int right) {
-        while (left < right) {
+        while(left < right) {
             char tmp = chars[left];
             chars[left] = chars[right];
             chars[right] = tmp;
             left = left + 1;
-            right = right -1;
+            right = right - 1;
         }
     }
+//     public String reverseWords(String s) {
+//         char[] chars = s.toCharArray();
+//         reverse(chars, 0, chars.length - 1);
+//         reverseWords(chars);
+//         return removeSpaces(chars);
+//     }
+//     private String removeSpaces(char[] chars) {
+//         int left = 0, right = 0;
+//         int len = chars.length;
+//         while (right < len) {
+//             while (right < len && Character.isSpace(chars[right])) {
+//                 right = right + 1;
+//             }
+//             while (right < len && Character.isLetter(chars[right])) {
+//                 chars[left] = chars[right];
+//                 left = left + 1;
+//                 right = right + 1;
+//             }
+//             while (right < len && Character.isSpace(chars[right])) {
+//                 right = right + 1;
+//             }
+//             if (right < len) {
+//                 chars[left] = ' ';
+//                 left = left + 1;
+//             }
+//         }
+        
+//         return String.valueOf(chars).substring(0, left);
+//     }
+    
+//     private void reverseWords(char[] chars) {
+//         int left = 0, right = 0;
+//         int len = chars.length;
+//         while (right < len) {
+//             while (left < len && Character.isSpace(chars[left])) {
+//                 left = left + 1;
+//             }
+//             right = left;
+//             while (right < len && Character.isLetter(chars[right])) {
+//                 right = right + 1;
+//             }
+//             reverse(chars, left, right - 1);
+//             left = right;
+//         }
+//     }
+    
+//     private void reverse(char[] chars, int left, int right) {
+//         while (left < right) {
+//             char tmp = chars[left];
+//             chars[left] = chars[right];
+//             chars[right] = tmp;
+//             left = left + 1;
+//             right = right - 1;
+//         }
+//     }
 }
