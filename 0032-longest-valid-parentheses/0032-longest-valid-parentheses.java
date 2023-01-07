2a25fc03394ad825427ec1a361fda0e9dfe9d1db
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2023-01-07 13:18:29 +0900

    Time: 2 ms (91.38%), Space: 42.6 MB (62.34%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -5,11 +5,14 @@ class Solution {
         stack.addLast(-1);
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (ch == '(') stack.addLast(i);
-            else {
+            if (ch == '(') {
+                stack.addLast(i);
+            } else {
                 stack.removeLast();
-                if (stack.isEmpty()) stack.addLast(i);
-                cnt = Math.max(i - stack.peekLast(), cnt);
+                if (stack.isEmpty()) {
+                    stack.addLast(i);
+                }
+                cnt = Math.max(cnt, i - stack.peekLast());
             }
         }
         return cnt;
