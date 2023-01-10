ea6aaa44907c288f96a8f9d6bd60b0b8b629d1e0
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2023-01-10 11:21:07 +0900

    Time: 0 ms (100.00%), Space: 43.2 MB (61.09%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,8 +3,10 @@ class Solution {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (nums[m] == target) return m;
-            else if (nums[m] < target) l = m + 1;
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] < target) l = m + 1;
             else r = m - 1;
         }
         return -1;
