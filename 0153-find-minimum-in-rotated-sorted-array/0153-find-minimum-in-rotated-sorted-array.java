1c0ec679367eacb40e9bdb372bfc7ecce2cfd336
chanoChoi <117348491+chanoChoi@users.noreply.github.com>
2023-01-10 12:18:19 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (84.33%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,19 +1,31 @@
 class Solution {
     public int findMin(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.min(nums[0], nums[1]);
-        if (nums[0] < nums[nums.length - 1]) return nums[0];
-        
-        int l = 0, r = nums.length - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (m + 1 < nums.length && nums[m] > nums[m + 1]) return nums[m + 1];
-            else if (m - 1 >= 0 && nums[m] < nums[m - 1]) return nums[m];
-            
-            if (nums[l] <= nums[m]) l = m + 1;
-            else if (nums[r] >= nums[m]) r = m - 1;
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.min(nums[0], nums[1]);
+        }
+        if (nums[0] < nums[nums.length - 1]) {
+            return nums[0];
         }
         
+        int l = 0, r = nums.length - 1;
+        int len = nums.length;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (m + 1 < len && nums[m] > nums[m + 1]) {
+                return nums[m + 1];
+            } else if (m - 1 >= 0 && nums[m] < nums[m - 1]) {
+                return nums[m];
+            }
+            
+            if (nums[l] <= nums[m]) {
+                l = m + 1;
+            } else if (nums[r] >= nums[m]){
+                r = m - 1;
+            }
+        }
         return 0;
     }
 }
